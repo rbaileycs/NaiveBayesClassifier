549e79180564826e4266d3707f150253cfd1d83e
Ryan Bailey <rbailey.cs@gmail.com>
2016-11-21 13:20:49 -0700

    got proper encoding to work for training data

--- naive_bayes.rb
+++ naive_bayes.rb
@@ -1,42 +1,24 @@
 require 'csv'
 require 'set'
 
-
-#puts "POLARITY : #{polarity} TEXT : #{text}"
-
-# CSV.foreach('testdata.csv') do |row|
-#   @polarity = row[0]
-#   @text = row[5]
-#
-# end
-
-
 # A module is a namespace and prevents name clashes
 module Bayes
 
-  class Base #this is where the parser should go
-
-    attr_accessor :polarity, :text, :container_array
-
-    def initialize_attributes
-
-    end
-
-    #end terminates the class
-
-    def tokenize(data)
-      data = data.split(/\W+/)
-      if data.first == ""
-        data = data.drop(1)
-      end
-      for index in 0...data.size
-        yield data[index]
-      end
-    end
-  end
+  # class Base #this is where the parser should go
+  #
+  #   attr_accessor :polarity, :text, :container_array
+  #
+  #   def initialize_attributes
+  #
+  #   end
+  #
+  #   #end terminates the class
+  #
+  #
+  # end
 
 # Naive extends Base
-  class Naive < Bayes::Base
+  class Naive #< Bayes::Base
 
     # Create attribute accessors, similar to get and set in Java
     attr_accessor :training_model, :classes
@@ -46,27 +28,42 @@ module Bayes
       # value are used in the invocation of a block
       @training_model = Hash.new { |h, k| h[k] = Hash.new(0) }
       @classes = Set.new
-      super # super call to class Bayes initialize_attributes
+      #super # super call to class Bayes initialize_attributes
     end
 
+    # This function trains the program by creating classes and
+    # a hashmap that contains the keys(classes) and
+    # values (associated strings).
     def train(category, data)
       @classes << category.to_sym
-      tokenize(data) do |data|
-        @training_model[category.to_sym][data] += 1
+      tokenize(data) { |data| @training_model[category.to_sym][data] += 1 }
+    end
+
+    # This function handles the initial parsing of the
+    # training data.
+    def training_parse(file)
+      CSV.foreach(file, :encoding =>'iso-8859-1') { |row|
+        category = row[0]
+        text = row[5] # discard the label
+        train category, text
+      }
+    end
+
+    # Tokenizes the initial parsing into separate words
+    def tokenize(data)
+        data = data.split(/\W+/)
+        if data.first == ''
+          data = data.drop(1)
+        end
+        (0...data.size).each { |index| yield data[index] }
       end
-
-    end
-
-    classifier = Naive.new
-    classifier.initialize_attributes
-
-    CSV.foreach('testdata.csv') do |row|
-      category = row[0]
-      text = row[5] # discard the label
-      classifier.train category, text
-
-    end
-    puts classifier.training_model.each_pair{|k, v| puts "Key: #{k}, Value: #{v}"}
   end
+
+  # Main logic goes here
+  classifier = Naive.new
+  classifier.initialize_attributes
+  classifier.training_parse('/Users/dev/Documents/School/training.csv')
+  puts classifier.training_model.each_pair { |k, v| puts "Key: #{k}, Value: #{v}" }
+
 end
 
